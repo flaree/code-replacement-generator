798fa5d17f2cfea469fbb7b42e48e8b3e4fe859a
flaree <flare2399@gmail.com>
2025-10-17 12:41:24 +0100

    add a buymeacoffee page

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,7 +18,19 @@ function About() {
 
   const paragraphStyle = {
     marginBottom: '15px',
-	color: '#fff',
+    color: '#fff',
+  };
+
+  const buttonStyle = {
+    display: 'inline-block',
+    marginTop: '20px',
+    padding: '10px 20px',
+    backgroundColor: '#FF813F', // Buy Me a Coffee orange color
+    color: '#fff',
+    textDecoration: 'none',
+    borderRadius: '5px',
+    fontWeight: 'bold',
+    fontSize: '16px',
   };
 
   return (
@@ -31,8 +43,17 @@ function About() {
         This app was created to simplify the process of generating photomechanic code replacements for football matches. By selecting teams from various leagues, users can quickly obtain the necessary codes for their workflows.
       </p>
       <p style={paragraphStyle}>It was a quick project with no thought/planning, just the info needed to make generating of the code replacements simple and easy.</p>
-	  <p style={paragraphStyle}>As information is sourced from transfermarkt website, this means there is no womens data available. I hope to make a womens version in the future.</p>
-      
+      <p style={paragraphStyle}>As information is sourced from transfermarkt website, this means there is no womens data available. I hope to make a womens version in the future.</p>
+      <p style={paragraphStyle}>After some requests i've added the below Buy Me a Coffee button, under no way should you feel obliged to do so. I made this project to solve some of the issues I occur while shooting matches and just want to share the tools I create.</p>
+
+      <a
+        href="https://www.buymeacoffee.com/cyqi5my0sl" 
+        target="_blank"
+        rel="noopener noreferrer"
+        style={buttonStyle}
+      >
+        Buy Me a Coffee
+      </a>
     </div>
   );
 }
