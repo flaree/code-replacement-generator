73f3071ce33adb58251bb660ec362d53533d1afd
flaree <flare2399@gmail.com>
2026-01-20 14:46:25 +0000

    fix error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TeamCodeGenerator from './pages/TeamCodeGenerator';
 import ManualClubSearch from './pages/ManualClubSearch';
 import About from './pages/About';
 import PhotoMetadata from './pages/PhotoMetadata';
-import ThemeToggle from './components/ThemeToggle';
+// import ThemeToggle from './components/ThemeToggle';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faChrome, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 
